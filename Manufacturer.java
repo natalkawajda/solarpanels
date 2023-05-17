62512f87caf67e26d825c0d6bd65e096ac161734
Marcin Tyminski <tymin12@o2.pl>
2023-05-17 11:35:49 +0200

    Manufacturer

---
+++ Manufacturer.java
@@ -0,0 +1,54 @@
+public class Manufacturer {
+
+  private String name;
+  private String country;
+  private String phone;
+  private String email;
+
+  public Manufacturer(String name, String country, String phone, String email){
+    this.name = name;
+    this.country = country;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getCountry()
+  {
+    return country;
+  }
+
+  public void setCountry(String country)
+  {
+    this.country = country;
+  }
+
+  public String getPhone()
+  {
+    return phone;
+  }
+
+  public void setPhone(String phone)
+  {
+    this.phone = phone;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+}
